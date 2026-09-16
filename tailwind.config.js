c02a48f985db89fe1ca4a325763ee0c1decee9e7
vynexstudio <celeasdeejay@gmail.com>
2026-09-16 09:19:15 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
       colors: {
         brand: {
-          blue: "#0808F5",   // Azul Intenso Oficial
-          red: "#FF0808",    // Vermelho Destaque Oficial
-          bg: "#F5F6F8",     // Cinza Muito Claro
-          text: "#222222",   // Cinza Escuro Texto
+          blue: "#0808F5",
+          red: "#FF0808",
+          bg: "#F5F6F8",
+          text: "#222222",
         },
       },
     },
